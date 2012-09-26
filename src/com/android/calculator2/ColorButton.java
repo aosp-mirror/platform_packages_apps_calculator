f4d403c1cf2a9abe231ec6e92fbca757bb8db592
mindyp <mindyp@google.com>
2012-09-26 13:42:41 -0700

    Dont draw white border on buttons
    just removed all the code that did it
    fixes b/7216478 Calculator uses non-standard touch feedback
    
    Change-Id: Ib8c918c4393f9898c67d74157588773d0d58cae5

--- src/com/android/calculator2/ColorButton.java
+++ src/com/android/calculator2/ColorButton.java
@@ -17,14 +17,10 @@
 package com.android.calculator2;
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Paint.Style;
 import android.util.AttributeSet;
 import android.view.View.OnClickListener;
 import android.view.View;
-import android.view.MotionEvent;
 import android.widget.Button;
 
 /**
@@ -39,12 +35,10 @@ class ColorButton extends Button implements OnClickListener {
     float mTextY;
     long mAnimStart;
     OnClickListener mListener;
-    Paint mFeedbackPaint;
 
     public ColorButton(Context context, AttributeSet attrs) {
         super(context, attrs);
         Calculator calc = (Calculator) context;
-        init(calc);
         mListener = calc.mListener;
         setOnClickListener(this);
     }
@@ -52,19 +46,6 @@ class ColorButton extends Button implements OnClickListener {
     public void onClick(View view) {
         mListener.onClick(this);
     }
-
-    private void init(Calculator calc) {
-        Resources res = getResources();
-
-        CLICK_FEEDBACK_COLOR = res.getColor(R.color.magic_flame);
-        mFeedbackPaint = new Paint();
-        mFeedbackPaint.setStyle(Style.STROKE);
-        mFeedbackPaint.setStrokeWidth(2);
-        getPaint().setColor(res.getColor(R.color.button_text));
-
-        mAnimStart = -1;
-    }
-
 
     @Override
     public void onSizeChanged(int w, int h, int oldW, int oldH) {
@@ -80,59 +61,5 @@ class ColorButton extends Button implements OnClickListener {
     @Override
     protected void onTextChanged(CharSequence text, int start, int before, int after) {
         measureText();
-    }
-
-    private void drawMagicFlame(int duration, Canvas canvas) {
-        int alpha = 255 - 255 * duration / CLICK_FEEDBACK_DURATION;
-        int color = CLICK_FEEDBACK_COLOR | (alpha << 24);
-
-        mFeedbackPaint.setColor(color);
-        canvas.drawRect(1, 1, getWidth() - 1, getHeight() - 1, mFeedbackPaint);
-    }
-
-    @Override
-    public void onDraw(Canvas canvas) {
-        if (mAnimStart != -1) {
-            int animDuration = (int) (System.currentTimeMillis() - mAnimStart);
-
-            if (animDuration >= CLICK_FEEDBACK_DURATION) {
-                mAnimStart = -1;
-            } else {
-                drawMagicFlame(animDuration, canvas);
-                postInvalidateDelayed(CLICK_FEEDBACK_INTERVAL);
-            }
-        } else if (isPressed()) {
-            drawMagicFlame(0, canvas);
-        }
-
-        CharSequence text = getText();
-        canvas.drawText(text, 0, text.length(), mTextX, mTextY, getPaint());
-    }
-
-    public void animateClickFeedback() {
-        mAnimStart = System.currentTimeMillis();
-        invalidate();
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        boolean result = super.onTouchEvent(event);
-
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_UP:
-                if (isPressed()) {
-                    animateClickFeedback();
-                } else {
-                    invalidate();
-                }
-                break;
-            case MotionEvent.ACTION_DOWN:
-            case MotionEvent.ACTION_CANCEL:
-                mAnimStart = -1;
-                invalidate();
-                break;
-        }
-
-        return result;
     }
 }
